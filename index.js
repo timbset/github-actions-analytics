a47442daf6d5fe9faee8708145d6251229b2984c
timbset <timbset@gmail.com>
2023-06-08 00:56:23 +0400

    Add loading and splitting jobs

--- index.js
+++ index.js
@@ -65,4 +65,93 @@ yargs(hideBin(process.argv))
       workflow_runs: runs,
     }, null, 2));
   })
+  .command('jobs load', 'Loads jobs using workflow run data', (yargs) => {
+    return yargs;
+  }, async () => {
+    const octokit = new Octokit({
+      auth: getEnv('GH_AUTH_TOKEN'),
+    });
+
+    const monday = new Date();
+    monday.setUTCHours(0, 0, 0);
+    monday.setUTCDate(monday.getUTCDate() - monday.getUTCDay() + 1);
+
+    const nextMonday = new Date(monday);
+    nextMonday.setUTCDate(nextMonday.getUTCDate() + 7);
+
+    const repoDirName = `${getEnv('GH_REPO_OWNER')}/${getEnv('GH_REPO_NAME')}`;
+    const dateDirName = monday.toISOString().split('T')[0];
+    const dataPath = path.join(fileURLToPath(new URL('.', import.meta.url)), 'data', repoDirName, dateDirName);
+
+    const workflowRunsPath = path.join(dataPath, 'workflow_runs.json');
+
+    if (!fs.existsSync(workflowRunsPath)) {
+      throw new Error('Workflow file not found');
+    }
+
+    const runs = JSON.parse(fs.readFileSync(workflowRunsPath).toString()).workflow_runs;
+
+    let status, data, jobs = [];
+
+    for (const run of runs) {
+      let page = 1;
+
+      do {
+        ({ status, data } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', {
+          owner: getEnv('GH_REPO_OWNER'),
+          repo: getEnv('GH_REPO_NAME'),
+          run_id: run.id,
+          per_page: 100,
+          page,
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28',
+          }
+        }));
+
+        page++;
+        jobs = jobs.concat(data.jobs);
+      } while (status === 200 && data.jobs.length >= 100);
+    }
+
+    fs.writeFileSync(path.join(dataPath, 'jobs.json'), JSON.stringify({
+      jobs,
+    }, null, 2));
+  })
+  .command('jobs split', 'Split jobs by workflow', (yargs) => {
+    return yargs;
+  }, async () => {
+    const monday = new Date();
+    monday.setUTCHours(0, 0, 0);
+    monday.setUTCDate(monday.getUTCDate() - monday.getUTCDay() + 1);
+
+    const nextMonday = new Date(monday);
+    nextMonday.setUTCDate(nextMonday.getUTCDate() + 7);
+
+    const repoDirName = `${getEnv('GH_REPO_OWNER')}/${getEnv('GH_REPO_NAME')}`;
+    const dateDirName = monday.toISOString().split('T')[0];
+    const dataPath = path.join(fileURLToPath(new URL('.', import.meta.url)), 'data', repoDirName, dateDirName);
+
+    const workflowRunsPath = path.join(dataPath, 'workflow_runs.json');
+
+    if (!fs.existsSync(workflowRunsPath)) {
+      throw new Error('Workflow file not found');
+    }
+
+    const runs = JSON.parse(fs.readFileSync(workflowRunsPath).toString()).workflow_runs;
+    const jobs = JSON.parse(fs.readFileSync(path.join(dataPath, 'jobs.json')).toString()).jobs;
+
+    const set = new Set();
+
+    runs.forEach((run) => {
+      set.add(run.name);
+    });
+
+    [...set.values()].forEach((name) => {
+      const wfJobs = jobs.filter((job) => job.workflow_name === name);
+
+      fs.writeFileSync(path.join(dataPath, `[jobs] ${name}.json`), JSON.stringify({
+        jobs: wfJobs,
+      }, null, 2));
+    });
+  })
   .parse();
