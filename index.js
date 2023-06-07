de8f69d91797f5f74c67eab52119f236f3638db1
timbset <timbset@gmail.com>
2023-06-07 22:00:00 +0400

    Implement using env

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
+import dotenv from 'dotenv';
+import { Octokit } from '@octokit/core';
+
+dotenv.config();
+
+const getEnv = (name) => {
+  if (process.env[name] == null) {
+    throw new Error(`Env ${name} is required`);
+  }
+
+  return process.env[name];
+};
 
 yargs(hideBin(process.argv))
   .scriptName('yarn analytics')
   .recommendCommands()
   .demandCommand(1)
-  .command('hello', 'Prints message', () => {
-    console.log('world');
+  .command('hello', 'Prints message', async () => {
+    const octokit = new Octokit({
+      auth: getEnv('GH_AUTH_TOKEN'),
+    });
+
+    const result = await octokit.request('GET /repos/{owner}/{repo}/actions/runs', {
+      owner: getEnv('GH_REPO_OWNER'),
+      repo: getEnv('GH_REPO_NAME'),
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+
+    console.log(result.data);
   })
   .parse();
