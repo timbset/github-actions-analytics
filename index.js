5006bd460b065096fe517a13973839a1cf62691a
timbset <timbset@gmail.com>
2023-06-12 16:40:25 +0400

    Improve jobs loading

--- index.js
+++ index.js
@@ -381,56 +381,80 @@ yargs(hideBin(process.argv))
   .command(
     'jobs load',
     'Loads jobs using workflow run data',
-    (yargs) => addFromAndToOptions(yargs),
-      async ({ from, to }) => {
+    (yargs) => addFromAndToOptions(yargs)
+      .option('workflow_file', {
+        description: 'Workflow file name',
+        type: 'string',
+        default: null,
+      }),
+    async ({ from, to, workflow_file }) => {
       const octokit = new Octokit({
         auth: getEnv('GH_AUTH_TOKEN'),
       });
 
-      const monday = new Date();
-      monday.setUTCHours(0, 0, 0);
-      monday.setUTCDate(monday.getUTCDate() - monday.getUTCDay() + 1);
+      const created = buildCreated(from, to);
+      const dataPath = path.join(fileURLToPath(new URL('.', import.meta.url)), 'data', getRepoDirName(), created);
+      const jobsPath = path.join(dataPath, 'jobs');
 
-      const nextMonday = new Date(monday);
-      nextMonday.setUTCDate(nextMonday.getUTCDate() + 7);
-
-      const repoDirName = `${getEnv('GH_REPO_OWNER')}/${getEnv('GH_REPO_NAME')}`;
-      const dateDirName = monday.toISOString().split('T')[0];
-      const dataPath = path.join(fileURLToPath(new URL('.', import.meta.url)), 'data', repoDirName, dateDirName);
-
-      const workflowRunsPath = path.join(dataPath, 'workflow_runs.json');
-
-      if (!fs.existsSync(workflowRunsPath)) {
-        throw new Error('Workflow file not found');
+      if (!fs.existsSync(jobsPath)) {
+        fs.mkdirSync(jobsPath);
       }
 
-      const runs = JSON.parse(fs.readFileSync(workflowRunsPath).toString()).workflow_runs;
+      let runIds = [];
 
-      let status, data, jobs = [];
+      if (workflow_file !== null) {
+        runIds = Array.isArray(workflow_file) ? workflow_file : [workflow_file];
+      } else {
+        const runsPath = path.join(dataPath, 'runs');
 
-      for (const run of runs) {
-        let page = 1;
+        if (!fs.existsSync(runsPath)) {
+          throw new Error('Workflow runs folder not found. Load runs first');
+        }
 
-        do {
-          ({ status, data } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', {
-            owner: getEnv('GH_REPO_OWNER'),
-            repo: getEnv('GH_REPO_NAME'),
-            run_id: run.id,
-            per_page: 100,
-            page,
-            headers: {
-              'X-GitHub-Api-Version': '2022-11-28',
-            }
-          }));
-
-          page++;
-          jobs = jobs.concat(data.jobs);
-        } while (status === 200 && data.jobs.length >= 100);
+        const runFiles = fs.readdirSync(path.join(dataPath, 'runs'));
+        runIds = runFiles.map((name) => name.replace('.json', ''));
       }
 
-      fs.writeFileSync(path.join(dataPath, 'jobs.json'), JSON.stringify({
-        jobs,
-      }, null, 2));
+      for (const runId of runIds) {
+        const runs = JSON.parse(fs.readFileSync(path.join(dataPath, 'runs', `${runId}.json`)).toString()).workflow_runs;
+        console.log(`"${runId}" workflow. Loading jobs for ${runs.length} workflow runs...`);
+
+        let status, data, jobs = [];
+
+        for (let i = 0; i < runs.length; i++) {
+          const run = runs[i];
+          let page = 1;
+          console.log(`  ${i + 1}/${runs.length} run jobs loading...`);
+
+          do {
+            ({ status, data } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', {
+              owner: getEnv('GH_REPO_OWNER'),
+              repo: getEnv('GH_REPO_NAME'),
+              run_id: run.id,
+              per_page: 100,
+              page,
+              headers: {
+                'X-GitHub-Api-Version': '2022-11-28',
+              }
+            }));
+
+            jobs = jobs.concat(data.jobs);
+
+            const current = Math.min(page * 100, data.total_count);
+            console.log(`    ${current}/${data.total_count} jobs loaded`);
+
+            page++;
+          } while (status === 200 && data.jobs.length >= 100);
+
+          console.log(`  ${i + 1}/${runs.length} run jobs loaded...`);
+        }
+
+        fs.writeFileSync(path.join(dataPath, 'jobs', `${runId}.json`), JSON.stringify({
+          jobs,
+        }, null, 2));
+
+        console.log(`"${runId}" workflow jobs saved`);
+      }
     }
   )
   .command('jobs split', 'Split jobs by workflow', async () => {
